70a4e6c8ad71d0d0798a3922f5b1f02fc887f525
Jesper Almström <jesper.almstrom@consultant.volvo.com>
2024-08-14 16:34:59 +0200

    forgot to read in output format

--- lambda/report_generator/report_generator.py
+++ lambda/report_generator/report_generator.py
@@ -11,11 +11,12 @@ def lambda_handler(event, context):
 
     report_bucket = os.getenv('BUCKET_NAME')
     inspector_report_cmk = os.getenv('KMS_KEY')
+    output_format = os.getenv('OUTPUT_FORMAT')
     now = datetime.datetime.now() # we could also use "time" from event
     # Partition the data into year,month and day
     key_prefix = f'year={now.year}/month={now.month}/day={now.day}/'
     response = inspector2_client.create_findings_report(
-        reportFormat='CSV',
+        reportFormat=output_format,
         s3Destination={
             'bucketName': report_bucket,
             'kmsKeyArn': inspector_report_cmk,
